527443379c3b86607836a111f40b60bffa50a49b
Kauan Portela <kauanphbbb@gmail.com>
2024-03-22 17:37:49 -0300

    feat: create the models

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-const port = 3001;
+    app.get('/', (req, res) => {
+      res.send('App is running!');
+    });
 
-app.get('/', (req, res) => {
-  res.send('App is running!');
-});
-
-app.listen(port, () => console.log(`🚀 Running on port ${port}`));
+    app.listen(port, () => console.log(`🚀 Running on port ${port}`));
+  })
+  .catch(() => console.log('Error connecting to MongoDB'));
