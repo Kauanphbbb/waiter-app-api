1f7f58c2c17751fb852fc87b7047b617ee7f0758
Kauan Portela <kauanphbbb@gmail.com>
2024-03-26 16:33:31 -0300

    feat: create change order status use case

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import path from 'node:path';
 import { createCategories } from './app/useCases/categories/createCategories';
 import { listCategories } from './app/useCases/categories/listCategories';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
+import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus';
 import { createOrder } from './app/useCases/orders/createOrder';
 import { listOrders } from './app/useCases/orders/listOrders';
 import { createProduct } from './app/useCases/products/createProduct';
@@ -37,9 +38,7 @@ router.get('/orders', listOrders);
 
 router.post('/orders', createOrder);
 
-router.patch('/orders/:orderId', (req, res) => {
-  res.send('OK');
-});
+router.patch('/orders/:orderId', changeOrderStatus);
 
 router.delete('/orders/:orderId', (req, res) => {
   res.send('OK');
