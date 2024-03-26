a6dcede78deaf618d4c0a1152fd1cb538480ad18
Kauan Portela <kauanphbbb@gmail.com>
2024-03-26 16:41:48 -0300

    feat: create cancel order use case

--- src/app/useCases/orders/changeOrderStatus.ts
+++ src/app/useCases/orders/changeOrderStatus.ts
@@ -6,17 +6,13 @@ export async function changeOrderStatus(req: Request, res: Response) {
     const { orderId } = req.params;
     const { status } = req.body;
 
-    if(!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)) {
+    if (!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)) {
       return res.status(400).json({
-        error: 'Status should be one of these: WAITING, IN_PRODUCTION or DONE'
+        error: 'Status should be one of these: WAITING, IN_PRODUCTION or DONE',
       });
     }
 
-    await Order.findByIdAndUpdate(
-      orderId,
-      { status },
-      { new: true }
-    );
+    await Order.findByIdAndUpdate(orderId, { status });
 
     res.sendStatus(204);
   } catch (error) {
